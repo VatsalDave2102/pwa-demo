ebe7a101637bc3a29c69b07bec22610b53c3a639
Vatsal Dave <vatsaldave2002@gmail.com>
2024-05-29 09:55:32 +0530

    (feat): precache static data

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -1,5 +1,6 @@
 var shareImageButton = document.querySelector("#share-image-button");
 var createPostArea = document.querySelector("#create-post");
+var sharedMomentsArea = document.querySelector("#shared-moments");
 var closeCreatePostModalButton = document.querySelector(
 	"#close-create-post-modal-btn"
 );
@@ -32,3 +33,34 @@ function closeCreatePostModal() {
 shareImageButton.addEventListener("click", openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener("click", closeCreatePostModal);
+
+// function to create a card
+function createCard() {
+	var cardWrapper = document.createElement("div");
+	cardWrapper.className = "shared-moment-card mdl-card mdl-shadow--2dp";
+	var cardTitle = document.createElement("div");
+	cardTitle.className = "mdl-card__title";
+	cardTitle.style.backgroundImage = "url('/src/images/sf-boat.jpg')";
+	cardTitle.style.backgroundSize = "cover";
+	cardTitle.style.height = "180px";
+	cardWrapper.appendChild(cardTitle);
+	var cardTitleTextElement = document.createElement("h2");
+	cardTitleTextElement.className = "mdl-card__title-text";
+	cardTitleTextElement.textContent = "San Fransisco Trip";
+	cardTitle.appendChild(cardTitleTextElement);
+	var cardSupportingText = document.createElement("div");
+	cardSupportingText.className = "mdl-card__supporting-text";
+	cardSupportingText.textContent = "In San Fransisco";
+	cardSupportingText.style.textAlign = "center";
+	cardWrapper.appendChild(cardSupportingText);
+	componentHandler.upgradeElement(cardWrapper);
+	sharedMomentsArea.appendChild(cardWrapper);
+}
+
+fetch("https://httpbin.org/get")
+	.then(function (res) {
+		return res.json();
+	})
+	.then(function (data) {
+		createCard();
+	});
--- public/sw.js
+++ public/sw.js
@@ -4,7 +4,29 @@
 // install and activate are triggered by install and activate
 self.addEventListener("install", function (event) {
 	console.log("[service worker] installing server worker", event);
+	// wait until pages and assets are cached, this is called precaching
+	event.waitUntil(
+		caches.open("static").then(function (cache) {
+			console.log("[service worker] precaching app shell");
+			cache.addAll([
+				"/",
+				"/index.html",
+				"/src/js/app.js",
+				"/src/js/feed.js",
+				"/src/js/promise.js",
+				"/src/js/fetch.js",
+				"/src/js/material.min.js",
+				"/src/css/app.css",
+				"/src/css/feed.css",
+				"/src/images/main-image.jpg",
+				"https://fonts.googleapis.com/css?family=Roboto:400,700",
+				"https://fonts.googleapis.com/icon?family=Material+Icons",
+				"https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
+			]);
+		})
+	);
 });
+
 self.addEventListener("activate", function (event) {
 	console.log("[service worker] activating server worker", event);
 	return self.clients.claim();
@@ -12,6 +34,16 @@ self.addEventListener("activate", function (event) {
 
 // fetch is triggered by web page
 self.addEventListener("fetch", function (event) {
-	console.log("[service worker] fetching something", event);
-	event.respondWith(fetch(event.request));
+	// here we are intercepting the fetch request to check if we already
+	// have the data in cache, if available, return cache
+	// if not, fetch data and return
+	event.respondWith(
+		caches.match(event.request).then(function (response) {
+			if (response) {
+				return response;
+			} else {
+				return fetch(event.request);
+			}
+		})
+	);
 });
