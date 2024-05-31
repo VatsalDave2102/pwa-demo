c6d6227847d3e4e80a6b8b83f27c152a16921771
VatsalDave2102 <124878757+VatsalDave2102@users.noreply.github.com>
2024-05-31 09:35:46 +0530

    (feat): add caching for application and offline availability (#3)
    * (feat): precache static data
    
    * (feat): add dynamic caching and error handling
    
    * (feat): add dynamic caching
    
    * (feat): add button to cache resource on demand
    
    * (feat): add advance caching and offline page

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -1,5 +1,6 @@
 var shareImageButton = document.querySelector("#share-image-button");
 var createPostArea = document.querySelector("#create-post");
+var sharedMomentsArea = document.querySelector("#shared-moments");
 var closeCreatePostModalButton = document.querySelector(
 	"#close-create-post-modal-btn"
 );
@@ -23,6 +24,15 @@ function openCreatePostModal() {
 
 		deferredPrompt = null;
 	}
+
+	// condition to unregister service workers
+	// if ("serviceWorker" in Navigator) {
+	// 	navigator.serviceWorker.getRegistration().then(function (registrations) {
+	// 		for (var i = 0; i < registrations.length; i++) {
+	// 			registrations[i].unregister();
+	// 		}
+	// 	});
+	// }
 }
 
 function closeCreatePostModal() {
@@ -32,3 +42,79 @@ function closeCreatePostModal() {
 shareImageButton.addEventListener("click", openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener("click", closeCreatePostModal);
+
+// currently not in use, allows to cache resource on user demand
+// const onSaveButtonClick = () => {
+// 	console.log("clicked");
+// 	// check if caches exist in window
+// 	if ("caches" in window) {
+// 		caches.open("user-requested").then(function (cache) {
+// 			cache.add("https://httpbin.org/get");
+// 			cache.add("/src/images/sf-boat.jpg");
+// 		});
+// 	}
+// };
+
+function clearCards() {
+	while (sharedMomentsArea.hasChildNodes()) {
+		sharedMomentsArea.removeChild(sharedMomentsArea.lastChild);
+	}
+}
+
+// function to create a card
+function createCard() {
+	var cardWrapper = document.createElement("div");
+	cardWrapper.className = "shared-moment-card mdl-card mdl-shadow--2dp";
+	var cardTitle = document.createElement("div");
+	cardTitle.className = "mdl-card__title";
+	cardTitle.style.backgroundImage = "url('/src/images/sf-boat.jpg')";
+	cardTitle.style.backgroundSize = "cover";
+	cardTitle.style.height = "180px";
+	cardWrapper.appendChild(cardTitle);
+	var cardTitleTextElement = document.createElement("h2");
+	cardTitleTextElement.style.color = "white";
+	cardTitleTextElement.className = "mdl-card__title-text";
+	cardTitleTextElement.textContent = "San Fransisco Trip";
+	cardTitle.appendChild(cardTitleTextElement);
+	var cardSupportingText = document.createElement("div");
+	cardSupportingText.className = "mdl-card__supporting-text";
+	cardSupportingText.textContent = "In San Fransisco";
+	cardSupportingText.style.textAlign = "center";
+	// var cardSaveButton = document.createElement("button");
+	// cardSaveButton.textContent = "Save";
+	// cardSaveButton.addEventListener("click", onSaveButtonClick);
+	// cardSupportingText.appendChild(cardSaveButton);
+	cardWrapper.appendChild(cardSupportingText);
+	componentHandler.upgradeElement(cardWrapper);
+	sharedMomentsArea.appendChild(cardWrapper);
+}
+
+var url = "https://httpbin.org/get";
+var networkDataReceived = false;
+fetch(url)
+	.then(function (res) {
+		return res.json();
+	})
+	.then(function (data) {
+		console.log("From web", data);
+		clearCards();
+		createCard();
+	});
+// cache then network strategy
+if ("caches" in window) {
+	caches
+		.match(url)
+		.then(function (response) {
+			if (response) {
+				return response.json();
+			}
+		})
+		.then(function (data) {
+			console.log("From cache", data);
+			// if data is received from web, dont create another card
+			if (!networkDataReceived) {
+				clearCards();
+				createCard();
+			}
+		});
+}
--- public/sw.js
+++ public/sw.js
@@ -1,17 +1,146 @@
 // when making changes in service worker, open a new tab to see changes or
 // update from developer console to see changes
 
+var CACHE_STATIC_NAME = "static-v12";
+var CACHE_DYNAMIC_NAME = "dynamic-v3";
+var STATIC_FILES = [
+	"/",
+	"/index.html",
+	"/offline.html",
+	"/src/js/app.js",
+	"/src/js/feed.js",
+	"/src/js/promise.js",
+	"/src/js/fetch.js",
+	"/src/js/material.min.js",
+	"/src/css/app.css",
+	"/src/css/feed.css",
+	"/src/images/main-image.jpg",
+	"https://fonts.googleapis.com/css?family=Roboto:400,700",
+	"https://fonts.googleapis.com/icon?family=Material+Icons",
+	"https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
+];
+
+// function to cleanup cache
+// function trimCache(cacheName, maxItems) {
+// 	caches.open(cacheName).then(function (cache) {
+// 		return cache.keys().then(function (keys) {
+// 			if (keys.length > maxItems) {
+// 				cache.delete(keys[0]).then(trimCache(cacheName, maxItems));
+// 			}
+// 		});
+// 	});
+// }
+
 // install and activate are triggered by install and activate
 self.addEventListener("install", function (event) {
 	console.log("[service worker] installing server worker", event);
+	// wait until pages and assets are cached, this is called precaching
+	event.waitUntil(
+		// change versions of cache when making changes in application, don't change service workers
+		// to update it in application to store latest files in cache
+		caches.open(CACHE_STATIC_NAME).then(function (cache) {
+			console.log("[service worker] precaching app shell");
+			cache.addAll(STATIC_FILES);
+		})
+	);
 });
+
 self.addEventListener("activate", function (event) {
 	console.log("[service worker] activating server worker", event);
+	event.waitUntil(
+		caches.keys().then(function (keyListt) {
+			return Promise.all(
+				keyListt.map(function (key) {
+					if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+						console.log("[Service worker] Rmoving old cache:", key);
+						return caches.delete(key);
+					}
+				})
+			);
+		})
+	);
 	return self.clients.claim();
 });
 
-// fetch is triggered by web page
+function isInArray(string, array) {
+	var cachePath;
+	if (string.indexOf(self.origin) === 0) {
+		// request targets domain we serve the page from
+		console.log("matched", string);
+
+		cachePath = string.substring(self.origin.length); // take the part of the URL after the domain
+	} else {
+		cachePath = string; // store the full request
+	}
+	return array.indexOf(cachePath) > -1;
+}
+
+// cache with network strategy
 self.addEventListener("fetch", function (event) {
-	console.log("[service worker] fetching something", event);
-	event.respondWith(fetch(event.request));
+	// for specific url, we use cache with network strategy
+	var url = "https://httpbin.org/get";
+	if (event.request.url.indexOf(url) > -1) {
+		event.respondWith(
+			// open the dynamic cache
+			caches.open(CACHE_DYNAMIC_NAME).then(function (cache) {
+				// fetch the request
+				return fetch(event.request).then(function (response) {
+					// trimCache(CACHE_DYNAMIC_NAME, 3);
+					// store the resposne in cache
+					cache.put(event.request, response.clone());
+					// return the response
+					return response;
+				});
+			})
+		);
+	} else if (isInArray(event.request.url, STATIC_FILES)) {
+		event.respondWith(caches.match(event.request));
+	} else {
+		// else use cache with network fallback strategy to find it in cache first, if not, then fetch
+		event.respondWith(
+			caches.match(event.request).then(function (response) {
+				if (response) {
+					return response;
+				} else {
+					return fetch(event.request)
+						.then(function (response) {
+							return caches.open(CACHE_DYNAMIC_NAME).then(function (cache) {
+								// trimCache(CACHE_DYNAMIC_NAME, 3);
+								cache.put(event.request.url, response.clone());
+								return response;
+							});
+						})
+						.catch(function (error) {
+							// returning cached offline page when request fails
+							return caches.open(CACHE_STATIC_NAME).then(function (cache) {
+								if (event.request.header.get("accept").includes("text/html")) {
+									return cache.match("/offline.html");
+								}
+							});
+						});
+				}
+			})
+		);
+	}
 });
+
+// cache only strategy
+// self.addEventListener("fetch", function (event) {
+// 	event.respondWith(caches.match(event.request));
+// });
+
+// network first then cache strategy
+// self.addEventListener("fetch", function (event) {
+// 	event.respondWith(
+// 		fetch(event.request)
+// 			.then(function (response) {
+// 				return caches.open(CACHE_DYNAMIC_NAME).then(function (cache) {
+// 					cache.put(event.request.url, response.clone());
+// 					return response;
+// 				});
+// 			})
+// 			.catch(function (error) {
+// 				return caches.match(event.request);
+// 			})
+// 	);
+// });
