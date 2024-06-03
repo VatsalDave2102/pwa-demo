1839b23ada6226d5bf5f42ce4a0468d85dd50197
Vatsal Dave <vatsaldave2002@gmail.com>
2024-06-03 15:34:13 +0530

    (feat): add backgorund sync to upload posts in offline status

--- public/src/js/utility.js
+++ public/src/js/utility.js
@@ -3,6 +3,9 @@ let dbPromise = idb.open("post-store", 1, function (db) {
 	if (!db.objectStoreNames.contains("posts")) {
 		db.createObjectStore("posts", { keyPath: "id" });
 	}
+	if (!db.objectStoreNames.contains("sync-posts")) {
+		db.createObjectStore("sync-posts", { keyPath: "id" });
+	}
 });
 
 // function to write data
